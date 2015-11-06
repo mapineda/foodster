ee7e5a2333d5b408a3b88f9c7160ef3435815d48
Robert Weilbaecher <robertweilbaecher@hotmail.com>
2015-11-06 14:30:02 -0600

    merge

--- restaurants/models.py
+++ restaurants/models.py
@@ -9,7 +9,11 @@ class Restaurant(models.Model):
     menu_url = models.CharField(max_length=200)
     description = models.CharField(max_length=250)
     pub_date = models.DateTimeField('date published')
-    search_fields = ['name']
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
     def __str__(self):
     	test = self.name + " " + self.address
     	return test
